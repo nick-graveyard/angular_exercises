f033de066cec951c4c5bafb25c8bcd113ec44da4
Nick Kiermaier <nick.kiermaier@ge.com>
2016-06-23 17:21:18 -0500

    First commit

---
+++ app/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  	$locationProvider.hashPrefix('!');
+
+	$routeProvider.when('/view1', {
+	    templateUrl: 'view1/view1.html',
+	  })
+
+	 .when('/view2', {
+	    templateUrl: 'view2/view2.html',
+	  })
+
+	.otherwise({redirectTo: '/view1'});
+}]);
---
+++ app/view1/view1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.view1', [])
+
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+
+
+	$scope.$watch('search', function() {
+      fetch();
+    });
+
+    $scope.search = "Hook";
+
+    function fetch(){
+    	$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      	.then(function(response){ $scope.details = response.data; });
+
+      	$http.get("http://www.omdbapi.com/?s=" + $scope.search)
+      	.then(function(response){ $scope.related = response.data; });
+    }
+}]);
